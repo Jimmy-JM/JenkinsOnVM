f05079d53b5e6fe8edece2d27da28e781631bfdf
Jongmin <jjongallee@hanmail.net>
2021-07-15 13:02:59 +0900

    6th

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like Kim Jongmin 5th');
+    res.send('I like Kim Jongmin BTS');
 });
 
 app.get('/start', function(req, res){
