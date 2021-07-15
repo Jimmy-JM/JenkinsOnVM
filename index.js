abc63aed0e32e359908ee62af99162a6e99087fc
Jongmin <jjongallee@hanmail.net>
2021-07-15 15:52:36 +0900

    2nd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like Jongmin');
+    res.send('엠피엠 인스톨');
 });
 
 app.get('/start', function(req, res){
